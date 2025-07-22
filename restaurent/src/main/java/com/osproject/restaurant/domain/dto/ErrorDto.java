8e989f8788f281ab876d931d767c6f8b05f97c76
osama khalid <Osam7ko>
2025-07-22 16:34:50 +0300

    Creating Controller Advice

---
+++ restaurent/src/main/java/com/osproject/restaurant/domain/dto/ErrorDto.java
@@ -0,0 +1,16 @@
+package com.osproject.restaurant.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorDto {
+
+    private Integer status;
+    private String message;
+}
