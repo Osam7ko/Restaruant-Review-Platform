801793362c83eacaabdbb94aea994008cb6efce9
osama khalid <Osam7ko>
2025-07-23 15:09:37 +0300

    adding more EXP

--- restaurent/src/main/java/com/osproject/restaurant/controller/ErrorController.java
+++ restaurent/src/main/java/com/osproject/restaurant/controller/ErrorController.java
@@ -6,14 +6,36 @@ import com.osproject.restaurant.exceptions.StorageException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 @RestController
 @ControllerAdvice
 @Slf4j
 public class ErrorController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error("Caught MethodArgumentNotValidException", ex);
+
+        String errorMessage = ex
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        ErrorDto errorDto = ErrorDto.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(errorMessage)
+                .build();
+
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(StorageException.class)
     public ResponseEntity<ErrorDto> handleStorageException(StorageException ex) {
@@ -27,7 +49,7 @@ public class ErrorController {
         return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(StorageException.class)
+    @ExceptionHandler(BaseException.class)
     public ResponseEntity<ErrorDto> handleSBaseException(BaseException ex) {
         log.error("Caught BaseException", ex);
 
